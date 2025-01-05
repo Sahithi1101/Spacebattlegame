448c51d361d8ead3aa45690339e3b52756ee8a48
sahithi1101 <sahithi1510@gmail.com>
2025-01-05 18:06:13 -0500

    spacebattle

---
+++ app.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var c = document.querySelector("canvas");
+    var canvas = document.querySelector("canvas");
+    c.width = innerWidth;
+    c.height = innerHeight;
+    c = c.getContext("2d");
+}
